717554c971ca4d845bb4cde483b68afda7793431
Radiv Lazuardi <96424060+Radiv0317@users.noreply.github.com>
2024-02-29 09:18:57 +0700

    home blade - add table view

--- resources/views/pages/home.blade.php
+++ resources/views/pages/home.blade.php
@@ -1,1 +1,22 @@
-
+<table>
+    <thead>
+        <tr>
+            <th>Kode Penerbangan</th>
+            <th>Waktu Keberangkatan</th>
+            <th>Waktu Sampai</th>
+            <th>Lokasi Keberangkatan</th>
+            <th>Lokasi Sampai</th>
+        </tr>
+    </thead>
+    <tbody>
+        @foreach($flight_schedules as $schedule)
+            <tr>
+                <td>{{ $schedule->flight_number }}</td>
+                <td>{{ $schedule->departure_time }}</td>
+                <td>{{ $schedule->arrival_time }}</td>
+                <td>{{ $schedule->departure_location }}</td>
+                <td>{{ $schedule->arrival_location }}</td>
+            </tr>
+        @endforeach
+    </tbody>
+</table>
