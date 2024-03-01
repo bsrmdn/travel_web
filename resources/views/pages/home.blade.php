d2ae0a889c4e75a420159cef7ac350c2438e715d
Bastiar <bastiarramadan76@gmail.com>
2024-03-01 10:29:37 +0700

    fix time logic

--- resources/views/pages/home.blade.php
+++ resources/views/pages/home.blade.php
@@ -28,18 +28,14 @@
                                 </thead>
                                 <tbody>
                                     @foreach ($courseSchedules as $schedule)
-                                        @if (
-                                            $schedule->waktu_mulai <= now('Asia/Jakarta')->toTimeString() &&
-                                                now('Asia/Jakarta')->toTimeString() <= $schedule->waktu_selesai)
-                                            <tr>
-                                                <td>{{ $schedule->kode_rombel }}</td>
-                                                <td>{{ $schedule->pelajaran }}</td>
-                                                <td>{{ $schedule->waktu_mulai }}</td>
-                                                <td>{{ $schedule->waktu_selesai }}</td>
-                                                <td>{{ $schedule->ruang }}</td>
-                                                <td>{{ $schedule->keterangan }}</td>
-                                            </tr>
-                                        @endif
+                                        <tr class="schedule-row">
+                                            <td>{{ $schedule->kode_rombel }}</td>
+                                            <td>{{ $schedule->pelajaran }}</td>
+                                            <td class="start-time">{{ $schedule->waktu_mulai }}</td>
+                                            <td class="end-time">{{ $schedule->waktu_selesai }}</td>
+                                            <td>{{ $schedule->ruang }}</td>
+                                            <td>{{ $schedule->keterangan }}</td>
+                                        </tr>
                                     @endforeach
                                     <p>{{ now('Asia/Jakarta')->toTimeString() }}</p>
                                 </tbody>
