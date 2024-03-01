82a55898314f23ad879ac43afb73da5f1357a187
Radiv0317 <maikelfapk@gmail.com>
2024-03-01 16:08:17 +0700

    commit

--- config/database.php
+++ config/database.php
@@ -45,10 +45,9 @@ return [
 
         'mysql' => [
             'driver' => 'mysql',
-            'url' => env('DATABASE_URL'),
             'host' => env('DB_HOST', '127.0.0.1'),
             'port' => env('DB_PORT', '3306'),
-            'database' => env('DB_DATABASE', 'forge'),
+            'database' => env('DB_DATABASE', 'travel_web'),
             'username' => env('DB_USERNAME', 'forge'),
             'password' => env('DB_PASSWORD', ''),
             'unix_socket' => env('DB_SOCKET', ''),
